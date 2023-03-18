15784238dda18438e8c37d078f0d6b05b5a9440a
woshyy <christopher.waatz@gmail.com>
2023-03-18 18:37:31 +0100

    testing

--- miryoku/miryoku_babel/current_layers.h
+++ miryoku/miryoku_babel/current_layers.h
@@ -8,7 +8,7 @@
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MT(LSHFT, A),     U_MT(RALT, S),     U_MT(LALT, D),    U_MT(RCTRL, F),    &kp G,             &kp H,             U_MT(RCTRL, J),    U_MT(LALT, K),    U_MT(RALT, L),     U_MT(LSHFT, APOS),   \
 U_LT(U_MEDIA, Z), U_MT(LGUI, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(LGUI, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, ESC),  U_LT(U_NUM, LALT),U_LT(U_SYM, RET),  &kp SPC, U_LT(U_FUN, DEL),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, ESC),  U_LT(U_NUM, TAB),U_LT(U_SYM, RET),  &kp SPC, U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_MEDIA_VI \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RGB_EFF,         U_RGB_HUI,         U_RGB_SAI,         U_RGB_BRI,         U_RGB_TOG,         \
@@ -26,7 +26,7 @@ U_NP,              U_NP,              &kp LPAR,          &kp LPAR,          &kp 
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
 U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp RC(BSPC),      &kp BSPC,         &kp PG_UP,         &kp END,           &kp INS,           \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp TAB,          &kp DEL,           U_NP,              U_NP
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp LCTRL,          &kp DEL,           U_NP,              U_NP
 
 
 #define MIRYOKU_ALTERNATIVES_NUM \
